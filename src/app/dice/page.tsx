6b30d057e62f9c8ab8772e5702dac034b60d1f3b
kamemarotamo <kamemarotamo@yahoo.co.jp>
2024-02-13 23:28:49 +0900

    十三日その２

---
+++ src/app/dice/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { useState } from "react"
+
+export default function Test() {
+    const [toriaezu, settoriaezu] = useState("");
+
+    return(
+        <>
+        
+        </>
+    )
+}
