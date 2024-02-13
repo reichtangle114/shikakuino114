4db3ab3b1df3f9572310dfee086e39b7f283c37b
kamemarotamo <kamemarotamo@yahoo.co.jp>
2024-02-14 02:06:17 +0900

    課題とりま

--- src/app/dice/page.jsx
+++ src/app/dice/page.jsx
@@ -1,12 +1,33 @@
 'use client'
 import { useState } from "react"
 
-export default function Test() {
-    const [toriaezu, settoriaezu] = useState("");
+export default function IchiDhyaku() {
+    const [diceroll, setdiceroll] = useState("");
+    var [rireki, setrireki] = useState([]);
+
+    function roll(){
+        let min = 1;
+        let max = 6;
+
+        let dorekana = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+
+        setdiceroll(dorekana)
+        setrireki([...rireki, diceroll])
+    }
+
+    function print(result) {
+        return (
+            <p>{result}</p>
+        )
+    }
 
     return(
         <>
-        
+            <p>{diceroll}</p>
+            <br></br>
+            <button type="button" onClick={roll}>ダイスを振る</button>
+            <br></br>
+            {rireki.map(print)}
         </>
     )
 }
